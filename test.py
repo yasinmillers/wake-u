ca64c42dba7a0045d6dc55358d23bb34b903f342
yasinmillers <ssenyonga100@gmail.com>
2025-11-12 08:41:28 +0300

    push it

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-for i in '127530964535':
-    pass
-print('Last i',i)
+class Dog:
+    species = "Canine"  # Class attribute
 
-class MyClass:
-    def method(self):
-        return "Hello, World!"
+    def __init__(self, name, color, age):
+        self.name = name  # Instance attribute
+        self.age = age  # Instance attribute
+        self.color = color  # Instance attribute
+
+dog1 = Dog("Buddy",  'blue',3)  # Create an instance of Dog
+dog2 = Dog("Charlie",'white', 5)# Create another instance of Dog
+dog3 = Dog("Max",'yellow', 2)    # Create a third instance of Dog
+
+
+print(dog1.name, dog1.age, dog1.species,dog1.color)  # Access instance and class attributes
+print(dog2.name, dog2.age, dog2.species,dog2.color)  # Access instance and class attributes
+print(Dog.species)  
