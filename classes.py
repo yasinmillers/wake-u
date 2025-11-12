f565020a9bd8fb2b1c5e804d42d1aa37c60b5f31
yasinmillers <ssenyonga100@gmail.com>
2025-11-12 11:32:15 +0300

    hggh

--- classes.py
+++ classes.py
@@ -21,4 +21,15 @@ class Dog(Animal):
         self.species = "Canine"  # Class attribute
 max= Dog("Max", "Brown", 4)
 print(max.name, max.age, max.species, max.color)  
+
+class Cat(Animal):
+    def __init__(self, name, color, age):
+        super().__init__(name, species="Feline", type=None, location=None, age=age, color=color, weight=None, height=None, gender=None, diet=None)  
+        self.name = name 
+        self.color = color 
+        self.age = age      
+        self.species = "Felineafrica"  # Class attribute
+bella = Cat("Bella", "Black", 3)
+print(bella.name, bella.age, bella.species, bella.color)
+           
                              
