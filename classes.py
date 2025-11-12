bea0e8a530e14a77b84e87770f2797ccecbba684
yasinmillers <ssenyonga100@gmail.com>
2025-11-12 10:56:02 +0300

    ..

--- classes.py
+++ classes.py
@@ -15,4 +15,5 @@ class Animal:
 cow = Animal("Bessie", "Mammal","Herbivore","Farm",5,"Brown",1500,60, " Female","Grass")
 goat = Animal("Billy", "Mammal","Herbivore","Farm",3,"White",200,30, "Male","Grass")
 cat = Animal("Whiskers", "Mammal","Carnivore","House",2,"Black",15,10, "Female","Fish")
+fox = Animal("Foxy", "Mammal","Omnivore","Forest",4,"Red",30,20, "Male","Small animals and fruits")
 print(cow.name, cow.species, cow.type, cow.location, cow.age, cow.color, cow.weight, cow.height, cow.gender, cow.diet)       
