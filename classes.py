f69999a8aa6461060fd26e4050ef707e5201f131
yasinmillers <ssenyonga100@gmail.com>
2025-11-12 10:30:55 +0300

    self

--- classes.py
+++ classes.py
@@ -2,6 +2,14 @@ class Animal:
     def __init__(self, name, species,type,location,age,color,weight,height,gender,diet):
         self.name = name
         self.species = species
+        self.type = type
+        self.location = location
+        self.age = age
+        self.color = color
+        self.weight = weight
+        self.height = height
+        self.gender = gender
+        self.diet = diet
 
     def speak(self):
         raise NotImplementedError("Subclasses must implement this method")
