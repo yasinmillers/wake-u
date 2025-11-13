db002d72a4ff733a3195de476c5b3ba2e1c34496
yasinmillers <ssenyonga100@gmail.com>
2025-11-13 08:51:49 +0300

    adding

---
+++ math.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+A = np.array([1, 2, 3])
+B= np.array([4, 5, 6])
+C= A + B# Element-wise addition of two arrays
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+d = a.dot(b)  # Matrix multiplication
+print(d)
+print(C)
