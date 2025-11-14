bd36529331e785e1c635044e4ac3b39220fdbc16
yasinmillers <ssenyonga100@gmail.com>
2025-11-14 13:01:22 +0300

    auto-commit

--- socketa.py
+++ socketa.py
@@ -1,8 +1,3 @@
 import socket
-def create_socket(host='localhost', port=8080):
-    """Create a TCP socket and connect to the specified host and port."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    
-    return s
-print("Socket created and connected to {}:{}".format('localhost', 8080))   
+ip = socket.gethostbyname('www.google.com')
+print (ip)
