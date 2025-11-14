3821decccff3e1eb7cc809bcec4eae31fb69d59a
yasinmillers <ssenyonga100@gmail.com>
2025-11-14 09:15:23 +0300

    sockets

---
+++ socketa.py
@@ -0,0 +1,6 @@
+import socket
+def create_socket(host='localhost', port=8080):
+    """Create a TCP socket and connect to the specified host and port."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    return s
