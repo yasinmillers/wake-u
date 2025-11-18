f095589119194d8b8836cd1748e4a7b29233eebe
yasinmillers <ssenyonga100@gmail.com>
2025-11-18 08:47:09 +0300

    square root of a number

---
+++ sq.py
@@ -0,0 +1,5 @@
+import math
+import numpy as np
+number = float(input("Enter a number to find its square root: "))
+sqrt_value = math.sqrt(number)
+print("The square root of {0} is {1}".format(number, sqrt_value))
