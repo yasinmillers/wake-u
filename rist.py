7702624cadf3cba909c3e316a127408b78c98fff
yasinmillers <ssenyonga100@gmail.com>
2025-11-20 20:53:14 +0300

    functions

--- rist.py
+++ rist.py
@@ -1,3 +1,10 @@
 this_list = ["apple", "banana", "cherry"]
 this_list[1:2] = ["blackcurrant", "watermelon"]
+this_list.insert(2, "orange")
+this_list.append("kiwi")
+this_list.remove("apple")
+this_list.sort()
+this_list.reverse()
+this_list.pop()
+this_list.clear()
 print(this_list)
