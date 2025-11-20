2db3ac7d15a9012eaef4172e1e26c734bf791b95
yasinmillers <ssenyonga100@gmail.com>
2025-11-20 10:18:30 +0300

    new

---
+++ ave.py
@@ -0,0 +1,9 @@
+# Get input from the user for the two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the average
+average = (num1 + num2) / 2
+
+# Print the result
+print("The average of", num1, "and", num2, "is:", average)
