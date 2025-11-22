d6eb624792544eb69fada1fd4162bace01b561bf
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 12:09:38 +0300

    nested2

--- cond1.py
+++ cond1.py
@@ -31,3 +31,19 @@ if age >= 18:
     print("You need a license")
 else:
   print("You are too young to drive")
+  
+  
+  score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
