d3fa2f95c696cddf08a2f0bf06a3fa27f032c374
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 12:13:44 +0300

    case

--- cond1.py
+++ cond1.py
@@ -47,3 +47,20 @@ if score >= 60:
     print("Pass but low attendance")
 else:
   print("Fail")
+  
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
