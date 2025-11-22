d512b5b066cc9f35393847f2467e151419c5c761
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 12:08:02 +0300

    nested

--- cond1.py
+++ cond1.py
@@ -21,3 +21,13 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
+  age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
