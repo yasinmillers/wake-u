96976cd132f035c0e401193225d26e031c29f8d0
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 12:03:51 +0300

    else

--- cond1.py
+++ cond1.py
@@ -8,3 +8,11 @@ elif age < 65:
   print("You are an adult")
 elif age >= 65:
   print("You are a senior")
+  a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
