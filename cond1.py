69292e6abb08a1eaf0b2c75c81c552212a6d5f52
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 12:16:09 +0300

    mmay

--- cond1.py
+++ cond1.py
@@ -64,3 +64,13 @@ match day:
     print("Saturday")
   case 7:
     print("Sunday")
+    
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")    
