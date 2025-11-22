e4375d330462e2fba060bf1d372830d3366db355
yasinmillers <ssenyonga100@gmail.com>
2025-11-22 11:59:52 +0300

    condition

---
+++ cond1.py
@@ -0,0 +1,10 @@
+age = 25
+
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
