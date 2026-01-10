b2279803833aaa68c26e7e559276b043687ce3b9
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 23:02:55 +0300

    count vwels

--- Exercise/no16.py
+++ Exercise/no16.py
@@ -1,1 +1,8 @@
-#
+#Count number of vowels in a string
+s = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+count = 0
+for char in s:
+    if char in vowels:
+        count += 1
+print("Number of vowels:", count)   
