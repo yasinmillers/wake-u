43885896d9d1b567b94d862521300ecec74da627
yasinmillers <ssenyonga100@gmail.com>
2026-01-11 00:08:58 +0300

    sap 2

--- Exercise/no30.py
+++ Exercise/no30.py
@@ -1,1 +1,7 @@
-#
+#Swap first and last characters of a string
+s = input("Enter a string: ")
+if len(s) > 1:
+    swapped = s[-1] + s[1:-1] + s[0]
+    print("String with first and last characters swapped:", swapped)
+else:
+    print("String has only one character or is empty.") 
