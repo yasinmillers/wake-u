302effaf8d8555597ccd73fe233617f862278a6b
yasinmillers <ssenyonga100@gmail.com>
2026-01-11 00:25:01 +0300

    laegest list

---
+++ Exercise/no32.py
@@ -0,0 +1,6 @@
+#Find largest and smallest element in list
+numbers = [1, 2, 3, 4, 5]
+largest = max(numbers)
+smallest = min(numbers)
+print("Largest element:", largest)
+print("Smallest element:", smallest)
