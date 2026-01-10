d4638b9b67946978fb8938227dce88fd6187c3ba
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 21:28:07 +0300

    4

---
+++ Exercise/no5.py
@@ -0,0 +1,12 @@
+#Check whether a year is a leap year
+year = int(input("Enter a year: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year")
