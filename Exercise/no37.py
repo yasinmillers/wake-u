9e3189343c64a2cea8f4d70b5b8398fa6e7f7437
yasinmillers <ssenyonga100@gmail.com>
2026-01-11 00:31:24 +0300

    largest

---
+++ Exercise/no37.py
@@ -0,0 +1,8 @@
+#Find second largest number in list
+numbers = [1, 2, 3, 4, 5]
+unique_numbers = list(set(numbers))
+unique_numbers.sort(reverse=True)
+if len(unique_numbers) >= 2:
+    print("Second largest number:", unique_numbers[1])
+else:
+    print("Not enough unique numbers")
