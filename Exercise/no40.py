355e554b972e6a55ea0da07a9cd78bf4f5608666
yasinmillers <ssenyonga100@gmail.com>
2026-01-11 00:37:22 +0300

    commn

---
+++ Exercise/no40.py
@@ -0,0 +1,5 @@
+#Find common elements between two lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+common_elements = list(set(list1) & set(list2))
+print("Common elements:", common_elements)
