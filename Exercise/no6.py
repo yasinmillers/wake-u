c79f1d11d738ea28cf9fd8871a38aa4cb64c0809
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 21:41:18 +0300

    intreset rate

--- Exercise/no6.py
+++ Exercise/no6.py
@@ -1,1 +1,6 @@
 # Calculate simple interest
+principal = float(input("Enter principal amount: "))
+rate = float(input("Enter rate of interest: "))
+time = float(input("Enter time period: "))
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest:", simple_interest)
