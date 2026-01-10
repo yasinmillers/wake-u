b53e7c8b0a95a48572d2e3782aad8b46d2950bdc
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 21:13:25 +0300

    correct

--- Exercise/no1.py
+++ Exercise/no1.py
@@ -1,8 +1,8 @@
 n = int(input("Enter a number: "))
 if n < 0:
     print("Negative number")
-else:
-    n>=0
+elif n >= 0:
     print("positive number")
-elif n==0:  
-    print("Zero")   
+else:
+    n == 0
+    print("Zero")  
