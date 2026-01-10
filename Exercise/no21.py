ae92599e39820109fc75eb3c2651cb2327f0f63d
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 23:24:39 +0300

    longest word

--- Exercise/no21.py
+++ Exercise/no21.py
@@ -1,1 +1,8 @@
-#
+#Find the longest word in a string
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+longest_word = ""
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+print("Longest word:", longest_word)
