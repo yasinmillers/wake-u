91881533efefe713925864901b261aa386e5d7ae
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 21:24:26 +0300

    small or big number

--- Exercise/no3.py
+++ Exercise/no3.py
@@ -1,6 +1,9 @@
-#Check whether a number is even or odd
-n = int(input("Enter a number: "))
-if n % 2 == 0:
-    print("Even number")
+#Find the largest of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: ")) 
+if a > b:
+    print("First number is largest")
+elif b > a:
+    print("Second number is largest")
 else:
-    print("Odd number")
+    print("Both numbers are equal") 
