efb7436410d0d6c2164d62ca866dc66f963bd9b7
yasinmillers <ssenyonga100@gmail.com>
2026-01-10 23:48:49 +0300

    contains digit

---
+++ Exercise/no28.py
@@ -0,0 +1,6 @@
+# Check whether string contains only digits
+s = input("Enter a string: ")
+if s.isdigit():
+    print("The string contains only digits.")
+else:
+    print("The string does not contain only digits.")
