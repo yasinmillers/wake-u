4214551ed721d46c44bcdf3308417ecfe9ffbef2
yasinmillers <ssenyonga100@gmail.com>
2026-01-20 11:56:21 +0300

    change

--- Exercise/no45.py
+++ Exercise/no45.py
@@ -1,18 +1,19 @@
 #Find pair whose sum is given number
 def find_pair(arr, target):
     num_dict = {}
-    for i, num in enumerate(arr):
+    for num in arr:
         complement = target - num
         if complement in num_dict:
-            return (num_dict[complement], i)
-        num_dict[num] = i
+            return (complement, num)
+        num_dict[num] = True
     return None
 
-arr= list(map(int, input("Enter numbers separated by spaces: ").split()))
-target = int(input("Enter the target sum: ")) 
+
+arr = list(map(int, input("Enter numbers separated by spaces: ").split()))
+target = int(input("Enter the target sum: "))
 
 result = find_pair(arr, target)
 if result:
-    print(f"Pair found at indices",result)
+    print("Pair found:", result)
 else:
     print("No pair found")
