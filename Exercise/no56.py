cb2f4ad8ee30a6c9eb7096933fcfc8f949c2fb4b
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 09:59:09 +0300

    sq2

--- Exercise/no56.py
+++ Exercise/no56.py
@@ -1,4 +1,6 @@
 #Create dictionary of square of numbers
 numbers = (1, 2, 3, 4, 5)
-square_dict = {x: x**2 for x in numbers}
+square_dict = {}
+for x in numbers:
+    square_dict[x] = x**2
 print("Dictionary of squares:", square_dict)
