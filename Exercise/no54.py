98e5954f13815c00a5a55f97ddb4e32cba771c6c
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 09:40:56 +0300

    even

--- Exercise/no54.py
+++ Exercise/no54.py
@@ -1,4 +1,7 @@
 #Find all even numbers in tuple
 t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-even_numbers = tuple(x for x in t if x % 2 == 0)
-print("Even numbers in tuple:", even_numbers)
+even_numbers=[]
+for x in t:
+    if x % 2 == 0:
+        even_numbers.append(x)
+print("Even numbers in tuple:", even_numbers)   
