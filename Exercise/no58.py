722fa790e95fa0c319b21d12ac7132578856b5b4
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 11:10:46 +0300

    willc

--- Exercise/no58.py
+++ Exercise/no58.py
@@ -1,1 +1,4 @@
-# Find key with maximum value   
+# Find key with maximum value in dictionary
+data = {'a': 5, 'b': 2, 'c': 9, 'd': 1}
+max_key = max(data, key=data.get)
+print("Key with maximum value:", max_key)
