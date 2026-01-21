90fb806dd7bf0ee95c52eedbe0f49ad5d3911f60
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 09:53:17 +0300

    seco

--- Exercise/nno55.py
+++ Exercise/nno55.py
@@ -1,4 +1,6 @@
 #Convert tuple of digits into a number
 t = (1, 2, 3, 4, 5)
-number = int(''.join(map(str, t)))
-print("Number:", number)    
+number=0
+for digit in t:
+    number = number * 10 + digit
+print("Number:", number)
