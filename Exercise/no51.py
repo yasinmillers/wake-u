2ccc622984fba2f3e44bbe78cd8b5d98861a8a41
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 09:17:01 +0300

    index

--- Exercise/no51.py
+++ Exercise/no51.py
@@ -1,1 +1,8 @@
-#
+# Find index of an element in tuple
+t = (1, 2, 3, 4, 5)
+element = int(input("Enter an element to find index: "))
+try:
+    index = t.index(element)
+    print("Index of element:", index)
+except ValueError:
+    print("Element not found in tuple")
