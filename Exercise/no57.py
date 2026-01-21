9788d177da89aeee4bcd82c503a5181a368ae12e
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 10:05:44 +0300

    freq

--- Exercise/no57.py
+++ Exercise/no57.py
@@ -1,6 +1,10 @@
 #Count frequency of characters using dictionary
-text = "hello"
+text = input("Enter a string: ")
 freq_dict = {}
 for char in text:
-    freq_dict[char] = freq_dict.get(char, 0) + 1
-print("Frequency dictionary:", freq_dict)
+    if char in freq_dict:
+        freq_dict[char] += 1
+    else:
+        freq_dict[char] = 1
+        
+print("Character frequency dictionary:", freq_dict)
