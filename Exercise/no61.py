b591c5bc494c8a162d607718e77a833111054f25
yasinmillers <ssenyonga100@gmail.com>
2026-01-21 15:46:15 +0300

    key

---
+++ Exercise/no61.py
@@ -0,0 +1,9 @@
+#Check if a key exists in a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+key = input("Enter a key to check: ")
+
+if key in my_dict:
+    print("Key exists")
+else:
+    print("Key does not exist")
