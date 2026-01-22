275210b1259c336eb7dfacba52472ae0011d09f6
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 10:42:16 +0300

    continue

--- Exercise/no72.py
+++ Exercise/no72.py
@@ -1,20 +1,21 @@
 #Grade system based on marks
-try:                                                    
-    marks = float(input("Enter marks: "))
-    if marks >= 90:
-        grade = "A+"
-    elif marks >= 80:
-        grade = "A"
-    elif marks >= 70:
-        grade = "B"
-    elif marks >= 60:
-        grade = "C"
-    elif marks >= 50:
-        grade = "D"
-    elif marks >= 0:
-        grade = "F"
-    else:
-        grade = "invalid marks"
-    print("Grade:", grade)  
-except ValueError:
-    print("Please enter a valid number for marks.") 
+while True:
+    try:                                                    
+        marks = float(input("Enter marks: "))
+        if marks >= 90:
+            grade = "A+"
+        elif marks >= 80:
+            grade = "A"
+        elif marks >= 70:
+            grade = "B"
+        elif marks >= 60:
+            grade = "C"
+        elif marks >= 50:
+            grade = "D"
+        elif marks >= 0:
+            grade = "F"
+        else:
+            grade = "invalid marks"
+        print("Grade:", grade)  
+    except ValueError:
+        print("Please enter a valid number for marks.") 
