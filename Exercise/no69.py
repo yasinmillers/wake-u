2929b91f777d53833aa9c69fa39f5bee194eaf5e
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 10:04:45 +0300

    highest

---
+++ Exercise/no69.py
@@ -0,0 +1,9 @@
+#Find highest scoring student
+n=int(input("Enter number of students: "))
+student_marks = {}
+for i in range(n):
+    name = input("Enter student name: ")
+    marks = float(input("Enter student marks: "))
+    student_marks[name] = marks
+highest_student = max(student_marks, key=student_marks.get)
+print("Highest Scoring Student:", highest_student)
