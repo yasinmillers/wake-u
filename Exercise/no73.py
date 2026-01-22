ace6667c540ba8cfaf7578d8d444d9d75eed5c19
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 11:24:56 +0300

    bill

--- Exercise/no73.py
+++ Exercise/no73.py
@@ -1,20 +1,15 @@
 # Electricity bill calculation
-while True:
-    try:
+units = float(input("Enter number of units consumed in ugandan settings : "))
+if units < 0:
+    print("Invalid input")
             
-        units = float(input("Enter number of units consumed in ugandan settings : "))
-        if units < 0:
-            print("Invalid input")
-            continue
+fixed_charge = 5000
+bill=0
+if units <= 100:
+    bill = units * 600
+elif units <= 200:
+    bill = 100 * 600 + (units - 100) * 750
+else:
+    bill = 100 * 600 + 100 * 750 + (units - 200) * 1000
 
-        fixed_charge = 5000
-        bill=0
-        if units <= 100:
-            bill = units * 600
-        elif units <= 200:
-            bill = 100 * 600 + (units - 100) * 750
-        else:
-            bill = 100 * 600 + 100 * 750 + (units - 200) * 1000
-        print("Electricity Bill:", bill + fixed_charge) 
-    except ValueError:
-        print("Please enter a valid number for units.")
+print("Electricity Bill:", bill + fixed_charge)
