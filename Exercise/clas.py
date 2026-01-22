09989ac270ab41fe6cc66a8fbdae994260c58ee1
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 14:29:38 +0300

    class vall

--- Exercise/clas.py
+++ Exercise/clas.py
@@ -1,1 +1,10 @@
- 
+class MyClass:
+    my_variable = 10
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+m1 = MyClass("Alice", 30)
+m2= MyClass("Bob", 25)
+print(m1.name, m1.age)
+print(m2.name, m2.age)
+print(MyClass.my_variable)
