c9a27b80733f66764179696f5b39710fd411a3ce
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 11:38:48 +0300

    log

--- Exercise/no74.py
+++ Exercise/no74.py
@@ -2,7 +2,7 @@
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-if username == "admin" and password == "admin123":
+if username == "admin" and password == "1234":
     print("Login successful.")
 else:
     print("Invalid username or password.")  
