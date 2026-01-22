d1db34642e05367cdf01a6691ea0c5ed43d2320d
yasinmillers <ssenyonga100@gmail.com>
2026-01-22 14:50:44 +0300

    modifry

--- Exercise/clas.py
+++ Exercise/clas.py
@@ -3,11 +3,18 @@ class MyClass:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+    def speak(self):
+        return f"My name is {self.name} and I am {self.age} years old."
+        
 m1 = MyClass("Alice", 30)
 m2= MyClass("Bob", 25)
 print(m1.name, m1.age)
 print(m2.name, m2.age)
 print(MyClass.my_variable)
 MyClass.my_variable=20
-print(MyClass.my_variable)
+my_variable=30
+print(m1.my_variable)
+m1.name="Charlie"
+print(m1.name)
 
