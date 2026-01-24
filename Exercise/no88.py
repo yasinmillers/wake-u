dd38ada3f5697031bca2e0901bb6bba1fc4ae58d
yasinmillers <ssenyonga100@gmail.com>
2026-01-24 22:16:59 +0300

    88

--- Exercise/no88.py
+++ Exercise/no88.py
@@ -1,5 +1,3 @@
 #Write a single line of code using a ternary operator that assigns the string "Even" or
 #"Odd" to a variable result based on a user's input number.
-number = int(input("Enter a number: "))
-result = "Even" if number % 2 == 0 else "Odd"
-print(result)
+print("Even" if int(input("Enter a number: ")) % 2 == 0 else "Odd")
