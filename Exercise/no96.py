9221c612d6aab2cd411dcb620ea461940a85728e
yasinmillers <ssenyonga100@gmail.com>
2026-01-24 09:09:24 +0300

    them

--- Exercise/no96.py
+++ Exercise/no96.py
@@ -1,14 +1,18 @@
 '''The Digital Thermostat (Void Functions)
 Definition: A system that triggers a physical action (simulated) without returning a value.'''
-def set_thermostat(temperature):
-    if temperature < 60:
-        action = "Heating On"
-        print(f"Thermostat set to {temperature}°F. {action}.")
-    elif temperature > 75:
-        action = "Cooling On"
-    else:
-        action = "Thermostat is Optimal"
 
-    print(f"Thermostat set to {temperature}°F. {action}.")  
+
+def digital_thermostat(temperature):
+    if temperature > 30:
+        print("🔥 Cooling system activated")
+    elif temperature < 18:
+        print("❄️ Heating system activated")
+    else:
+        print("✅ Temperature is stable")
+
+
 # User input
-temp_input = input("Enter desired temperature (°F): ")  
+temp = int(input("Enter current temperature: "))
+
+# Call the void function
+digital_thermostat(temp)
