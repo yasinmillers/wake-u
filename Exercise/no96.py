4ae5ac42d7acad1ed121340c34c05fbaf8845f2e
yasinmillers <ssenyonga100@gmail.com>
2026-01-24 09:01:59 +0300

    thermo

--- Exercise/no96.py
+++ Exercise/no96.py
@@ -1,1 +1,13 @@
-#
+'''The Digital Thermostat (Void Functions)
+Definition: A system that triggers a physical action (simulated) without returning a value.'''
+def set_thermostat(temperature):
+    if temperature < 60:
+        action = "Heating On"
+    elif temperature > 75:
+        action = "Cooling On"
+    else:
+        action = "Thermostat is Optimal"
+
+    print(f"Thermostat set to {temperature}°F. {action}.")  
+# User input
+temp_input = input("Enter desired temperature (°F): ")  
