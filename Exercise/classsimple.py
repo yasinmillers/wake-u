3f5c4ff071c805dbca99d6c6d3b6324aa5cee4b1
yasinmillers <ssenyonga100@gmail.com>
2026-01-29 14:41:24 +0300

    se

--- Exercise/classsimple.py
+++ Exercise/classsimple.py
@@ -7,11 +7,15 @@ class B(A):
     def display(self):
         print("This is class B")  
 
-
+class C(A, B):
+    def display(self):
+        print("This is class C")
 
 obj_a = A()
 obj_a.display()  
 obj_b = B()
 obj_b.display()  
+obj_c = C()
+obj_c.display()
 
-              
+            
