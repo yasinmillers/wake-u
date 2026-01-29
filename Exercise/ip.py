1582685e74af6e517b25633195ea33dc8e0e191c
yasinmillers <ssenyonga100@gmail.com>
2026-01-29 17:08:53 +0300

    auto-commit

--- Exercise/ip.py
+++ Exercise/ip.py
@@ -1,3 +1,10 @@
 #print a ip adresses from 192.168.1.0 to 192.168.1.255
+import random
 for i in range(0, 256):
     print(f"192.168.1.{i}")
+
+
+ports = list(range(8000, 8101)) 
+for i in range(0, 256):
+    for port in ports:
+        print(f"192.168.1.{i}:{port}")  
