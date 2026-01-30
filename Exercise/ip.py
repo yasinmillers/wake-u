e27a5a00af42ed7a6ea7814f37936e6a7b398d31
yasinmillers <ssenyonga100@gmail.com>
2026-01-30 09:43:11 +0300

    ports

--- Exercise/ip.py
+++ Exercise/ip.py
@@ -1,10 +1,8 @@
 #print a ip adresses from 192.168.1.0 to 192.168.1.255
-import random
-for i in range(0, 256):
-    print(f"192.168.1.{i}")
+ports = [80, 443, 8080, 3000, 5000, 8000, 8100]
 
 
-ports = list(range(8000, 8101)) 
+
 for i in range(0, 256):
     for port in ports:
         print(f"192.168.1.{i}:{port}")  
