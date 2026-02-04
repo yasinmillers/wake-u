e4f8580d996a156e3e8187a2bb4693ff1c577a2e
yasinmillers <ssenyonga100@gmail.com>
2026-02-04 11:55:43 +0300

    errors

--- Exercise/nameerr.py
+++ Exercise/nameerr.py
@@ -1,3 +1,11 @@
 #14+ pam*3  
 
-1 '2'+4
+#1 '2'+4
+
+try:
+    x=int(input("Enter a number: "))
+    pass
+
+
+except ValueError: 
+    print("An error occurred: Invalid input. Please enter a valid integer.")
