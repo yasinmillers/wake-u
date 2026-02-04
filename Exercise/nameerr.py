421d66ec9fa3e0af3e364f3959010d15fb4d419f
yasinmillers <ssenyonga100@gmail.com>
2026-02-04 12:09:06 +0300

    canot be dived zero

--- Exercise/nameerr.py
+++ Exercise/nameerr.py
@@ -2,10 +2,18 @@
 
 #1 '2'+4
 
-try:
+'''try:
     x=int(input("Enter a number: "))
     pass
 
 
 except ValueError: 
-    print("An error occurred: Invalid input. Please enter a valid integer.")
+    print("An error occurred: Invalid input. Please enter a valid integer.")'''
+    
+n=10
+try:
+    result=n/0
+    print(result)
+except ZeroDivisionError:
+    print("An error occurred: Division by zero is not allowed.")
+    
