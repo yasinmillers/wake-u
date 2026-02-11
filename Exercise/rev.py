cf6054700f8bf9f58fd5a235d77a0122074dac62
yasinmillers <ssenyonga100@gmail.com>
2026-02-11 16:44:39 +0300

    okk

--- Exercise/rev.py
+++ Exercise/rev.py
@@ -1,14 +1,18 @@
 # Create a class
+from unicodedata import name
+
+
 class Person:
    def __init__(self,name,age):
+       
         self.name=name
         self.age=age
 
    def greet(self):
-        print("Hello,my name is",name)
+       print("Hello,my name is",self.name)
 
 
-
+    
 # Create an object
 p1=Person("john",5)
 
