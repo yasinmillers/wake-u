9b2964abb5b2fd5346a621b984374566934100c0
yasinmillers <ssenyonga100@gmail.com>
2026-02-12 15:29:51 +0300

    k

--- logtail.py
+++ logtail.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 #Sample log data
 
-log_data = ["192.168.1.10", "192.168.1.15", "192.168.1.10", "192.168.1.10", "192.168.1.25","8.8.8.8", "10.0.0.1", "172.16.0.1"]
+log_data = ["192.168.1.10", "192.168.1.15","192.168.1.25","1.1.1.1","192.168.1.10", "192.168.1.10", "192.168.1.25","8.8.8.8","8.8.8.8","10.0.0.1", "172.16.0.1"]
 
 freq=Counter(log_data)
 print(freq)
@@ -10,3 +10,14 @@ print(freq)
 for ip, count in freq.items():
     print(f"IP Address: {ip} - Count: {count}")
   
+print("Most common IP address:", freq.most_common(1))
+
+print("\n short tail")
+for ip, count in freq.items():
+    if count >1:
+        print(ip)
+        
+print("\n long tail")
+for ip, count in freq.items():
+    if count ==1:
+        print(ip)
