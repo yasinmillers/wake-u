9640d9e76585f7e7b860e2ba26e6e15d0a83dbc4
yasinmillers <ssenyonga100@gmail.com>
2026-02-22 15:59:48 +0300

    change

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -1,2 +1,3 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
