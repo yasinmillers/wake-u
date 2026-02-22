56214790c2931b13d20f955adb46c3f86e230e83
yasinmillers <ssenyonga100@gmail.com>
2026-02-22 15:16:02 +0300

    space

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -1,3 +1,12 @@
 age = 36
 txt = f"My name is John, I am {age}"
 print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+txt = "We are the so-called \\Vikings\\ from the north."
+print(txt)
+txt = "We are the so-called \nVikings from the north."
+print(txt)
+txt = "We are the so-called \tVikings from the north."
+print(txt)
