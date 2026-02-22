4eab35909df26c02fee879660032745461c2565a
yasinmillers <ssenyonga100@gmail.com>
2026-02-22 17:00:51 +0300

    auto-commit

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -1,3 +1,3 @@
-thislist = ["apple", "banana", "cherry"]
-thislist[1] = "blackcurrant"
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
