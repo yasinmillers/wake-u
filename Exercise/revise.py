61b0603966fd22b900aba4c8c0dce199c52b277b
yasinmillers <ssenyonga100@gmail.com>
2026-02-22 15:39:59 +0300

    ere

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -1,12 +1,5 @@
-age = 36
-txt = f"My name is John, I am {age}"
-print(txt)
+fruits = ["apple", "banana", "cherry"]
 
-txt = "We are the so-called \"Vikings\" from the north."
-print(txt)
-txt = "We are the so-called \\Vikings\\ from the north."
-print(txt)
-txt = "We are the so-called \nVikings from the north."
-print(txt)
-txt = "We are the so-called \tVikings from the north."
-print(txt)
+print("banana" in fruits)
+print("grape" in fruits)
+print(fruits[0:2])
