9c002399e4a9918f5c9651f9bcbe7c0eb435e386
yasinmillers <ssenyonga100@gmail.com>
2026-02-23 10:23:35 +0300

    evencount

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -8,7 +8,16 @@ for i in range(5):
 # Find min and max
 minimum = min(numbers)
 maximum = max(numbers)
+even_count=0
+odd_count=0
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1  
 
 print("List:", numbers)
 print("Minimum value:", minimum)
 print("Maximum value:", maximum)
+print("Even numbers count:", even_count)
+print("Odd numbers count:", odd_count)
