79b7dc48284730ca14e7a7dd1ca5b94c6701b0ae
yasinmillers <ssenyonga100@gmail.com>
2026-02-23 08:09:57 +0300

    overide

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -12,3 +12,16 @@ class Child(Parent):
 c = Child() 
 # child calls overridden method
 c.myMethod()
+
+class example:
+    def add(self, a, b):
+      x = a+b
+      return x
+    def add(self, a, b, c):
+      x = a+b+c
+      return x
+
+obj = example()
+
+print (obj.add(10,20,30))
+print (obj.add(10,20))
