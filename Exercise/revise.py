78ec468bf82a70e7878576ec79673fba4e5ad165
yasinmillers <ssenyonga100@gmail.com>
2026-02-23 09:48:06 +0300

    largeset

--- Exercise/revise.py
+++ Exercise/revise.py
@@ -1,7 +1,7 @@
 numbers = [1, 2, 3, 4, 5]
 unique_numbers = list(set(numbers))
-unique_numbers.sort(reverse=True)
+unique_numbers.sort()
 if len(unique_numbers) >= 2:
-    print("Second largest number:", unique_numbers[1])
+    print("Second largest number:", unique_numbers[-2])
 else:
     print("Not enough unique numbers")
