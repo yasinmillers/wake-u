53a7944fd1d36fbf5aed7a964ef3833015d66fb1
yasinmillers <ssenyonga100@gmail.com>
2026-02-26 15:05:20 +0300

    reading image

--- Exercise/fileu.py
+++ Exercise/fileu.py
@@ -9,7 +9,33 @@ file.close()'''
 with open("new.txt","r") as file:
     data=file.read()
     print(data) '''
-    
-file=open("new.txt","r")
+    # Using with statement to read the file line by line
+'''file=open("new.txt","r")
 for line in file:
-    print(line) 
+    print(line) '''
+    
+    #reading a file and storing its content in a list
+'''with open("new.txt","r") as file:
+    lines=file.readlines()
+    print(lines) '''
+    
+    # Writing to a file
+'''with open("new.txt","w") as file:
+    file.write("This is a new line.\n")
+    file.write("This is another line.\n") '''
+    
+    # Appending to a file
+'''with open("new.txt","a") as file:
+    file.write("This line is appended.\n") '''
+    
+    # Using try-except to handle file not found error
+'''try:
+    with open("nonexistent.txt","r") as file:
+        data=file.read()
+        print(data)
+except FileNotFoundError:
+    print("The file does not exist.") '''
+    
+with open("images.jpeg","rb") as file:
+    data=file.read(100)
+    print(data)
