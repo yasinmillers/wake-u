4eb93ba0922adeee00f1469f3717a57250f0d51c
yasinmillers <ssenyonga100@gmail.com>
2026-03-06 15:35:13 +0300

    tabler

--- Exercise/dbconnectdemo.py
+++ Exercise/dbconnectdemo.py
@@ -2,3 +2,8 @@ import sqlite3
 
 conn=sqlite3.connect('mydatabase.db')
 cursor=conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  name TEXT NOT NULL,
+                  email TEXT NOT NULL)''')   
