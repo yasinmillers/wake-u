20229b5c3d985939c45768bfdac552a8eb08bdf3
yasinmillers <ssenyonga100@gmail.com>
2026-03-06 15:39:42 +0300

    sql

--- Exercise/dbconnectdemo.py
+++ Exercise/dbconnectdemo.py
@@ -1,9 +1,17 @@
 import sqlite3
 
-conn=sqlite3.connect('mydatabase.db')
+conn=sqlite3.connect('student.db')
 cursor=conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS users
+cursor.execute('''CREATE TABLE IF NOT EXISTS students
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  name TEXT NOT NULL,
-                  email TEXT NOT NULL)''')   
+                  name TEXT ,
+                  course TEXT ,
+                  marks INTEGER ,
+                  email TEXT )
+                  ''')  
+
+cursor.execute("INSERT INTO students (name, course, marks, email) VALUES ('John Doe', 'Python', 85, 'john.doe@example.com')")       
+cursor.execute("INSERT INTO students (name, course, marks, email) VALUES ('Jane Smith', 'Data Science', 92, 'jane.smith@example.com')")
+cursor.execute("INSERT INTO students (name, course, marks, email) VALUES ('Alice Johnson', 'Web Development', 78, 'alice.johnson@example.com')")    
+conn.commit()
