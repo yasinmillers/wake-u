63c56f526274386c73bfde5cfe4979e5d67c5a46
yasinmillers <ssenyonga100@gmail.com>
2026-04-17 08:02:17 +0300

    flder

--- os.py
+++ os.py
@@ -11,3 +11,15 @@ for item in items:
         print("file:", item)
     elif os.path.isdir(file):
         print("directory:", item)
+        
+        
+        
+print(" Current WorkingDirectory:")
+print(os.getcwd())
+print("\ Files in CurrentDirectory:")
+files = os.listdir()
+for file in files:
+    if os.path.isfile(file):
+        print(" File:", file)
+else:
+    print(" Folder:", file)
